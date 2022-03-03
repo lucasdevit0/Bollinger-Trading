e95f7d237c78f13d87651fe27a292e423e3f01fb
Lucas Casa de Vito <100882796+LucasCasaVito@users.noreply.github.com>
2022-03-03 20:36:06 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from binance.client import Client
 
 # Binance API Keys
 def API_keys():
-    api_key = "aJT1idslE4VFdlGiqTNMiwDd6UExuRyviJiViRyRI1ZX0Q3uDZTy7Ek50lMfPD9P"
-    api_secret = "WIPOuCCsQlqKPFFlJNyX8LcTwH1ACkoMRvsTTOzGPD8fMidzCySZ5aQDfV2EFw8E"
+    api_key = ""
+    api_secret = ""
 
     #Create client
     client = Client(api_key, api_secret)
